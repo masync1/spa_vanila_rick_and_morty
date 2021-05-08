f6b7f1862051299f1c3aa8ef9f48b53e571460ac
Juan manuel <juan.manuel.masync@protonmail.com>
2021-05-07 21:40:11 -0500

    En la rama master template characters

---
+++ src/pages/characters.js
@@ -0,0 +1,21 @@
+const character = () => {
+    return `
+    <section class="characters_inner">
+        <article class="characters_card">
+            <img src="image" alt="name">
+            <h2>name</h2>
+        </article>
+        <article class="characters_card">
+            <h3>Episodes:</h3>
+            <h3>Estatus:</h3>
+            <h3>Species:</h3>
+            <h3>Gender:</h3>
+            <h3>Origin:</h3>
+            <h3>Last location:
+            </h3>
+        </article>
+    </section>
+    `
+}
+
+export default character
---
+++ src/templates/header.js
@@ -0,0 +1,16 @@
+const header = () => {
+    const view = `
+    <section class="header_main">
+        <div class="header_logo">
+            <h1>
+                <a href="/"></a>
+                SPA r&m
+            </h1>
+        </div>
+        <div class="header_about">
+            <a href="#/about/"> About</a>
+        </div>
+    </section>
+    `
+    return view
+}
